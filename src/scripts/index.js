d9386c6ce5af69b691d51fa311dddedd0c599f50
moganda12 <115957608+moganda12@users.noreply.github.com>
2024-01-07 21:14:32 +0000

    time to move

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,13 +22,19 @@ camera.position.set(10, 10, 10);
 const firstperson = new PointerLockControls( camera, renderer.domElement );
 
 const textureLoader = new THREE.TextureLoader();
-const dirt = './assets/minecraft/textures/block/dirt.png';
+function loadTexture(path) {
+    let texture = textureLoader.load(path);
+    texture.magFilter = THREE.NearestFilter;
+    return texture;
+}
+
+const dirt = loadTexture('./assets/minecraft/textures/block/dirt.png');
 
 const axis = new THREE.AxesHelper(5);
 scene.add(axis);
 
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshStandardMaterial({color: 0xFFFFFF, map: textureLoader.load(dirt)});
+const boxMaterial = new THREE.MeshStandardMaterial({color: 0xFFFFFF, map: dirt});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 
 scene.add(box);
@@ -43,14 +49,89 @@ scene.add(dirLight);
 dirLight.position.set(1,1,1);
 
 
-renderer.domElement.addEventListener('click', function() {
+renderer.domElement.addEventListener('click', () => {
     firstperson.lock();
-})
+});
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let moveUp = false;
+let moveDown = false;
+
+document.addEventListener('keydown', (kevent) => {
+    switch (kevent.code) {
+        case 'ArrowUp':
+        case 'keyW':
+            moveForward = true;
+            break;
+        case 'ArrowDown':
+        case 'keyS':
+            moveBackward = true;
+            break;
+        case 'ArrowLeft':
+        case 'keyA':
+            moveLeft = true;
+            break;
+        case 'ArrowRight':
+        case 'keyD':
+            moveRight = true;
+            break;
+        case 'keyR':
+            moveUp = true;
+            break;
+        case 'keyF':
+            moveDown = true;
+            break;
+        default:
+            break;
+    };
+});
+
+document.addEventListener('keyup', (kevent) => {
+    switch (kevent.code) {
+        case 'ArrowUp':
+        case 'keyW':
+            moveForward = false;
+            break;
+        case 'ArrowDown':
+        case 'keyS':
+            moveBackward = false;
+            break;
+        case 'ArrowLeft':
+        case 'keyA':
+            moveLeft = false;
+            break;
+        case 'ArrowRight':
+        case 'keyD':
+            moveRight = false;
+            break;
+        case 'keyR':
+            moveUp = false;
+            break;
+        case 'keyF':
+            moveDown = false;
+            break;
+        default:
+            break;
+    };
+});
 
 let dt = 0.01;
 let lastTime = new Date().getTime();
 
 function renderGame(time) {
+    let velocity = new THREE.Vector3();
+    let velz = moveBackward - moveForward;
+    let velx = moveRight - moveLeft;
+    let vely = moveDown - moveUp;
+    let v3dmul = Math.sqrt(velz+velx+vely);
+    velx *= v3dmul;
+    vely *= v3dmul;
+    velz *= v3dmul;
+    velocity.set(velx,vely,velz);
+    camera.position += velocity;
     dt = time - lastTime;
     renderer.render(scene, camera);
     lastTime = time;
