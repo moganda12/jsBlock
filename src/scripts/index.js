db83d059459d9a54f28affe433a3e4162e762840
moganda12 <115957608+moganda12@users.noreply.github.com>
2024-01-09 15:52:41 +0000

    save myself

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -130,41 +130,15 @@ let lastTime = performance.now();
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshLambertMaterial({map: dirt});
 
-const cellSize = 256;
-const cell = new Uint8Array(cellSize * cellSize * cellSize);
-
-for (let y = 0; y < cellSize; ++y) {
-    for (let z = 0; z < cellSize; ++z) {
-        for (let x = 0; x < cellSize; ++x) {
-            const height = (Math.sin(x / cellSize * Math.PI * 4) + Math.sin(z / cellSize * Math.PI * 6)) * 20 + cellSize / 2;
-            if (height > y && height < y + 1) {
-                const offset = y * cellSize * cellSize +
-                               z * cellSize +
-                               x;
-                cell[offset] = 1;
-            };
-        };
+class Chunk {
+    constructor(x,y,z) {
+        this.size = 32;
+        this.content = new Uint16Array(this.size**3);
     };
 };
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhongMaterial({color: 'green'});
-
-for (let y = 0; y < cellSize; ++y) {
-    for (let z = 0; z < cellSize; ++z) {
-        for (let x = 0; x < cellSize; ++x) {
-            const offset = y * cellSize * cellSize +
-                         z * cellSize +
-                         x;
-            const block = cell[offset];
-            if(block === 1) {
-                const mesh = new THREE.Mesh(geometry, material);
-                mesh.position.set(x, y, z);
-                scene.add(mesh);
-            };
-        };
-    };
-};
+const material = new THREE.MeshStandardMaterial({texture: dirt});
 
 function renderGame() {
     stats.begin();
